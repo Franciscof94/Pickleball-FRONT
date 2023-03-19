3a178cd1f2bbe5b286a8bdc6faa18bde2d8b27c0
Francisco Ferraro <fran94_ferraro@hotmail.com>
2023-03-18 23:13:25 -0300

    show errors

--- app/home-page.tsx
+++ app/home-page.tsx
@@ -37,6 +37,7 @@ export default function Page({ allShifts }: Props) {
   const onSubmit = async (data: IShift) => {
     try {
       const res = await postShift(data);
+      console.log("FIJARSE ACA", res)
       dispatch(setShiftSuccess(true))
     } catch (error) {
       console.log(error);
